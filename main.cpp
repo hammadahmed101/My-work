95f6a3d553f5f60546abcacbfac87561300f3d2c
11hammadahmad22@gmail.com <blahblah@gmail.com>
2025-12-03 22:12:03 +0500

    Hello

--- main.cpp
+++ main.cpp
@@ -9,11 +9,6 @@ cout << " Hello World1" << endl;
 cout << " Hello World2" << endl;
 cout <<
 
-cout << " Hello World" << endl;
-cout << " Hello World1" << endl;
-cout << " Hello World2" << endl;
-cout <<
-
 
     return 0;
 }
